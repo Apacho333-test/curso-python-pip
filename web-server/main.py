4817c05f29e9a4ebfa7a5bc13bec1af2b0c96e33
Apacho333-test <apacho333@gmail.com>
2024-12-06 17:10:46 -0500

    docker y paginas web

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,21 @@
 import store
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app =FastAPI()
+
+@app.get("/") # un decorador, la ruta donde se ingresa a la web
+def get_list():
+    return[1,2,3,]
+
+@app.get("/contact", response_class= HTMLResponse)
+def get_list():
+    #return{"name": "Platzi"} para cargar solo un texto el html (crear un servidor web con python) es para cargar una pagina completa y editarla
+    return """<h1> Hola soy una pagina </h1>
+<p> soy un parrafo </p>"""
+
+
 def run():
     store.get_categories()
 
